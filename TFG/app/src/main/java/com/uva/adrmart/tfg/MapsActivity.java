1e886687e62c8e28694d9dc34d0e2314c27be2e8
Adrian <adrmartgom@gmail.com>
2016-02-26 19:17:58 +0100

    Custom_infowindow para los marker del mapa

--- TFG/app/src/main/java/com/uva/adrmart/tfg/MapsActivity.java
+++ TFG/app/src/main/java/com/uva/adrmart/tfg/MapsActivity.java
@@ -4,8 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -47,32 +49,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng valladolid = new LatLng(41.646929, -4.716680);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.plaza);
-        mMap.addMarker(new MarkerOptions()
+        final MarkerOptions marker = new MarkerOptions()
                 .position(valladolid)
                 .title("Marker in Valladolid")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                .snippet("Subtitulo")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        mMap.addMarker(marker);
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
 
+            private View myContestsView;
+
             public View getInfoWindow(Marker arg0) {
-                View v = getLayoutInflater().inflate(R.layout.custom_infowindow, null);
-                ImageView v1 = (ImageView) v.findViewById(R.id.image1);
-                ImageView v2 = (ImageView) v.findViewById(R.id.image2);
-                ImageView v3 = (ImageView) v.findViewById(R.id.image3);
+                return null;
+            }
+
+            public View getInfoContents(Marker arg0) {
+                myContestsView = getLayoutInflater().inflate(R.layout.custom_infowindow, null);
+                ImageView v1 = (ImageView) myContestsView.findViewById(R.id.image1);
+                ImageView v2 = (ImageView) myContestsView.findViewById(R.id.image2);
+                ImageView v3 = (ImageView) myContestsView.findViewById(R.id.image3);
+
+                TextView titulo = (TextView) myContestsView.findViewById(R.id.titulo);
+                Log.d("MIERDA", marker.getTitle());
+                titulo.setText(marker.getTitle());
+                TextView subtitulo = (TextView) myContestsView.findViewById(R.id.subtitulo);
+                Log.d("PUTA MIERDA", marker.getSnippet());
+                subtitulo.setText(marker.getSnippet());
+                Log.d("PUTA MIERDA", (String)titulo.getText());
+/*
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.plaza);
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                float scaleWidth = ((float) 400) / width;
+                float scaleHeight = ((float) 400) / height;
+                // create a matrix for the manipulation
+                Matrix matrix = new Matrix();
+                // resize the bit map
+                matrix.postScale(scaleWidth, scaleHeight);
+                // recreate the new Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, false);
+                v1.setImageBitmap(bitmap);
+                v2.setImageBitmap(bitmap);
+                v3.setImageBitmap(bitmap);*/
 
                 v1.setImageResource(R.drawable.plaza);
                 v2.setImageResource(R.drawable.plaza);
                 v3.setImageResource(R.drawable.plaza);
-                return v;
-            }
-
-            public View getInfoContents(Marker arg0) {
-
-                //View v = getLayoutInflater().inflate(R.layout.custom_infowindow, null);
-
-                return null;
-
+                return myContestsView;
             }
         });
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(valladolid, 10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(valladolid, 15));
     }
 }
